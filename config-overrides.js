b284090d129c2cd48d9d515c5f7813649275a820
yogntao.li <yongtao.li@williamoneilchina.com>
2020-12-09 19:06:56 -0800

    add eslint for code

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
-
 const rewireSass = require('react-app-rewire-scss');
+
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    config = rewireSass(config, env);
-    // config = rewireSass.withLoaderOptions(someLoaderOptions)(config, env);
-    return config;
-  }
+  // do stuff with the webpack config...
+  config = rewireSass(config, env);
+  // config = rewireSass.withLoaderOptions(someLoaderOptions)(config, env);
+  return config;
+};
